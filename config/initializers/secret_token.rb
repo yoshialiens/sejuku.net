fc3eb21979a9ba2075d79230fa1f12444ff2407c
Shota Kiuchi <shota7180@gmail.com>
2013-09-11 01:12:18 +0900

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sejuku::Application.config.secret_token = 'c47740e6dd1d121e95b249ca6a623b5ebcd2b18c38eda82e2de33ee2de6d78bcc1f78188bb2dfbb401397fa91611d3fa6a90f6f27a336c4993f09e57f5b70bc5'
