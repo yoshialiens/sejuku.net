700ae132ac3418fbe503de615ba137f5463b06ee
Shota Kiuchi <shota7180@gmail.com>
2013-10-22 07:50:17 +0900

    Test step mails

--- config/environment.rb
+++ config/environment.rb
@@ -9,5 +9,5 @@ ActionMailer::Base.smtp_settings = {
 	:address => "huv.sakura.ne.jp",
 	:port => 587,
 	:authentication => :plain,
-	:user_name => "侍エンジニア塾.net",
+	:user_name => "huv.sakura.ne.jp",
 	:password => 'Akibakim0i'}
