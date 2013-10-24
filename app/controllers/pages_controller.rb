dca8306fd97f55edbc5a61a68ba29e649e9b7534
木内 翔大 <shota7180@kinashi-shouoo-no-MacBook-Air.local>
2013-10-24 16:31:19 +0900

    Add pages checks

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,8 @@
 class PagesController < ApplicationController
   layout "lesson", :only => [:lesson]
+
+	def checks
+		@users = User.where("created_at > ?", Time.mktime(2013, 10, 8, 0, 0, 0)).order("created_at asc")
+		@user_agents = UserAgent.where("created_at > ?", Time.mktime(2013, 10, 21, 20, 0, 0)).order("created_at desc").limit(100)
+	end
 end
