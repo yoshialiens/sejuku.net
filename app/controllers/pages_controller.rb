2edfdd4b51a587624113dea91e8147c946befa5b
木内 翔大 <shota7180@kinashi-shouoo-no-MacBook-Air.local>
2013-10-24 16:41:42 +0900

    Fix pages#checks

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,6 @@ class PagesController < ApplicationController
 
 	def checks
 		@users = User.where("created_at > ?", Time.mktime(2013, 10, 8, 0, 0, 0)).order("created_at asc")
-		@user_agents = UserAgent.where("created_at > ?", Time.mktime(2013, 10, 21, 20, 0, 0)).order("created_at desc").limit(100)
+		@user_agents = UserAgent.where("created_at > ?", Time.mktime(2013, 10, 21, 20, 0, 0)).order("created_at desc").limit(1000)
 	end
 end
