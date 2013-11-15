26e626aea792441b8f29a6c09c54c6070a52c286
takuro yoshimi <hikago_uining@hotmail.com>
2013-11-15 21:16:17 +0900

    new commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,26 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+ after_filter :store_location
+
+def store_location
+ # 今回の場合は、 /users/sign_in , /users/sign_up, /users/password にアクセスしたとき、ajaxでのやりとりはsessionには保存しない。
+    if (request.fullpath != "/users/sign_in" && \
+        request.fullpath != "/users/sign_up" && \
+        request.fullpath != "/users/password" && \
+        !request.xhr?) # don't store ajax calls
+      session[:previous_url] = request.fullpath 
+    end
+end
+
+#ログイン後のリダイレクトをログイン前のページにする場合
+def after_sign_in_path_for(resource)
+  session[:previous_url] || root_path
+end
+
+#ログアウト後のリダイレクトをログアウト前のページにする場合
+def after_sign_out_path_for(resource)
+  session[:previous_url] || root_path
+end
+
 end
