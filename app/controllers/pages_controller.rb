44e92c68969c2d123697b8cff5f4e0a7fbd37ed8
takuro yoshimi <hikago_uining@hotmail.com>
2014-04-21 11:03:18 +0900

    interviewLP修正03

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -59,6 +59,11 @@ class PagesController < ApplicationController
     @directory = "/assets/base/"
   end
 
+  def interview
+    @title = "インタビュー：平井真哉さん"
+    @directory = "/assets/interview/"
+  end
+
 	def checks
 		@users = User.where("created_at > ?", Time.mktime(2013, 10, 8, 0, 0, 0)).order("created_at asc")
 		@user_agents = UserAgent.where("created_at > ?", Time.mktime(2013, 10, 21, 20, 0, 0)).order("created_at desc").limit(1000)
